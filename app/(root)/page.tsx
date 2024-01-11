dd254af799d93375ed1315505185ba43afafe3e0
MentorMindOtaku <contact@seckmamadou.com>
2024-01-11 10:44:22 +0100

    initialize banner section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from 'next';
 import { SanityDocument } from 'next-sanity';
 import { client, queries } from '@/sanity/lib';
 import { generateMetadataWithSanity } from '@/helper';
+import { Banner } from '@/components/pages/home';
 
 // Generate title and description from Sanity
 export async function generateMetadata(): Promise<Metadata> {
@@ -11,5 +12,9 @@ export async function generateMetadata(): Promise<Metadata> {
 export default async function Home() {
   const { banner } = await client.fetch<SanityDocument>(queries.GET_HOME_PAGE);
 
-  return <div>{banner.title}</div>;
+  return (
+    <main className='wrapper'>
+      <Banner />
+    </main>
+  );
 }
