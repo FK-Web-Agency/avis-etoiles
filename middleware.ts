35c983693a362b5afc7c6c25d2f951365bafac05
MentorMindOtaku <contact@seckmamadou.com>
2024-01-19 21:46:24 +0100

    edit public routes in middleware file

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from '@clerk/nextjs';
 
 // This middleware will run for every request
 export default authMiddleware({
-  publicRoutes: ["/api/webhooks(.*)"],
+  publicRoutes: ['/', '/about', '/contact', '/features', '/prices', '/studio(.*)', 'api/send', '/api/webhooks(.*)'],
 });
 
 export const config = {
