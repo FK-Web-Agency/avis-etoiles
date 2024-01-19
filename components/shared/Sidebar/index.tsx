cc1eece27597163bbb3e2a249584850909ec8076
MentorMindOtaku <contact@seckmamadou.com>
2024-01-19 05:16:50 +0100

    dashboard: change sidebar

--- components/shared/Sidebar/index.tsx
+++ components/shared/Sidebar/index.tsx
@@ -11,7 +11,7 @@ export default function Sidebar({ children }: PropsWithChildren) {
     <>
       <div>
         {/* Static sidebar for desktop */}
-        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-60 lg:flex-col">
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-600 px-6 pb-4">
             <div className="flex h-16 shrink-0 items-center">
@@ -38,7 +38,7 @@ export default function Sidebar({ children }: PropsWithChildren) {
           </div>
         </div>
 
-        <div className="lg:pl-72">
+        <div className="lg:pl-60">
           <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-600 px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
             <MobileNav />
             <div className="-m-2.5 p-2.5 text-gray-700 lg:hidden"></div>
