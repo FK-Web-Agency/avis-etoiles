5d87f1f823a74af2aeb965dea5920fcc0beb21e6
MentorMindOtaku <contact@seckmamadou.com>
2024-01-20 01:12:32 +0100

    edit midleware ingnoreRoutes

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { authMiddleware } from '@clerk/nextjs';
 // This middleware will run for every request
 export default authMiddleware({
   publicRoutes: ['/', '/about', '/contact', '/features', '/prices', '/studio(.*)', 'api/send', '/api/webhooks(.*)'],
-  ignoredRoutes:['/api/send', '/api/webhooks(.*)'],
+  ignoredRoutes:['/api/send', '/api/webhook/clerk', '/api/webhook/stripe'],
 });
 
 export const config = {
