585edb2f049dd6018ff405615823d44f9b392c33
MentorMindOtaku <contact@seckmamadou.com>
2024-02-07 05:33:29 +0100

    Refactor sign-in page appearance

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,15 +2,17 @@ import { SignIn } from '@clerk/nextjs';
 
 export default function Page() {
   return (
-    <SignIn
-      appearance={{
-        variables: {
-          colorPrimary: 'hsl(47.9, 95.8% ,53.1%)',
-        },
-        elements: {
-          footer: 'hidden',
-        },
-      }}
-    />
+    <div className="flex-center min-h-screen w-full background-body">
+      <SignIn
+        appearance={{
+          variables: {
+            colorPrimary: 'hsl(47.9, 95.8% ,53.1%)',
+          },
+          elements: {
+            footer: 'hidden',
+          },
+        }}
+      />
+    </div>
   );
 }
