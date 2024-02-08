2c465f5e643c421950ce28834207d373c45bc2a6
MentorMindOtaku <contact@seckmamadou.com>
2024-02-08 20:16:40 +0100

    Add feature section to home page

--- components/pages/home/Features.tsx
+++ components/pages/home/Features.tsx
@@ -29,6 +29,7 @@ const Item = ({ title, subtitle, icon }: FeatureProps['features'][0]) => {
 export default function Features({ features_section }: FeaturesType) {
   return (
     <section className="bg-amber-100">
+      feat
       <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
         <div className="max-w-screen-md mb-8 lg:mb-16">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">{features_section?.title} </h2>
