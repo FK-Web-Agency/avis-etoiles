c175a5167dee6de5ca3c95397d9d4157c4203f7a
MentorMindOtaku <contact@seckmamadou.com>
2024-02-12 21:34:07 +0100

    Refactor Stripe webhook route handling

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -1,79 +1,70 @@
 import stripe from 'stripe';
 import { NextResponse } from 'next/server';
-import { createOrder } from '@/lib/actions/order.actions';
-import { formatDate } from '@/helper';
-import updateUser from '@/sanity/lib/members/updateUser';
-import { client } from '@/sanity/lib';
+import { createOrder } from '@/lib/actions/order.actions'; // Assurez-vous que ce chemin est correct
+import { formatDate } from '@/helper'; // Utilisé pour formater les dates si nécessaire
+import updateUser from '@/sanity/lib/members/updateUser'; // Mise à jour des utilisateurs dans Sanity
+import { client } from '@/sanity/lib'; // Client Sanity pour les opérations de base de données
 
+// Fonction asynchrone pour gérer les requêtes POST
 export async function POST(request: Request) {
-  // Parse the request body
+  // Parsez le corps de la requête
   const body = await request.text();
 
-  // Retrieve the Stripe signature from the request headers
+  // Récupération de la signature Stripe depuis les en-têtes de la requête
   const sig = request.headers.get('stripe-signature') as string;
-  const endpointSecret = 'whsec_WxJevKl88mrxDGXgLJ1Xkf3gh4omPD2F';
-
-  let event;
-  let invoice = '';
-  let order: any = {};
+  const endpointSecret = 'whsec_WxJevKl88mrxDGXgLJ1Xkf3gh4omPD2F'; // Secret de l'endpoint, à garder sécurisé
 
   try {
-    // Construct the Stripe event using the request body, signature, and endpoint secret
-    event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
+    // Construction de l'événement Stripe à partir du corps de la requête, de la signature, et du secret de l'endpoint
+    const event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
+    let invoice = '';
+    let order: any = {};
+
+    // Traitement basé sur le type d'événement Stripe
+    switch (event.type) {
+      case 'invoice.payment_succeeded':
+        // Traitement pour le paiement d'une facture réussi
+        invoice = event.data.object.invoice_pdf as string;
+        break;
+      case 'checkout.session.completed':
+        // Traitement pour une session de paiement terminée
+        const { id, amount_total, metadata } = event.data.object;
+
+        const buyer = JSON.parse(metadata?.buyer as string);
+        const seller = JSON.parse(metadata?.seller as string);
+        const subscription = JSON.parse(metadata?.subscription as string);
+
+        // Mise à jour de l'abonnement de l'acheteur
+        subscription.status = true;
+        await updateUser({
+          id: buyer._ref,
+          user: subscription,
+        });
+
+        // Création de l'objet de commande
+        order = {
+          stripeId: id,
+          plan: metadata?.plan || '',
+          frequency: metadata?.frequency || '',
+          buyer,
+          seller,
+          price: amount_total,
+          createdAt: new Date().toISOString(),
+          invoice,
+        };
+        console.log('order', order, invoice);
+
+        // Création de la commande dans la base de données
+        const newOrder = await createOrder(order);
+
+        // Réponse de succès avec la nouvelle commande
+        return NextResponse.json({ message: 'OK', order: newOrder });
+    }
   } catch (err: any) {
-    // Handle webhook error
-    return NextResponse.json({ message: 'Webhook error', error: err });
+    // Gestion des erreurs liées aux webhooks
+    return NextResponse.json({ message: 'Webhook error', error: err.message });
   }
 
-  // Get the type of the Stripe event
-  const eventType = event.type;
-
-  if (eventType === 'invoice.payment_succeeded') {
-    const { invoice_pdf } = event.data.object;
-    console.log('invoice', order);
-
-    invoice = invoice_pdf as string;
-  }
-
-  // Handle 'checkout.session.completed' event
-  if (eventType === 'checkout.session.completed') {
-    const { id, amount_total, invoice, metadata } = event.data.object;
-    console.log(event.data.object);
-
-    const buyer = JSON.parse(metadata?.buyer as string);
-    const seller = JSON.parse(metadata?.seller as string);
-    const subscription = JSON.parse(metadata?.subscription as string);
-
-    console.log('req invoice', order);
-
-    // Create an order object
-    order = {
-      ...order,
-      stripeId: id,
-      plan: metadata?.plan || '',
-      frequency: metadata?.frequency || '',
-      buyer,
-      seller,
-      price: amount_total,
-      createdAt: new Date().toISOString(),
-    };
-
-    // Update the buyer's subscription
-    subscription.status = true;
-    await updateUser({
-      id: buyer._ref,
-      user: subscription,
-    });
-  }
-
-  console.log('order', order, invoice);
-
-  // Create the order in the database
-  const newOrder = await createOrder({ ...order, invoice });
-
-  // Return success response with the new order
-  return NextResponse.json({ message: 'OK', order: newOrder });
-
-  // Return empty response with 200 status
+  // Réponse vide avec le statut 200 si aucun traitement n'est effectué
   return new Response('', { status: 200 });
 }
