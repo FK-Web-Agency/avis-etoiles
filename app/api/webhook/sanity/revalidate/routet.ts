47eaa1ff17d8af0e72ad52123e4612d224d3212c
MentorMindOtaku <contact@seckmamadou.com>
2024-02-17 05:01:19 +0100

    Refactor revalidate route for webhook sanity

--- app/api/webhook/sanity/revalidate/routet.ts
+++ app/api/webhook/sanity/revalidate/routet.ts
@@ -1,44 +1,53 @@
-import { isValidSignature, SIGNATURE_HEADER_NAME } from '@sanity/webhook';
-import { NextRequest, NextResponse } from 'next/server';
-import { revalidatePath } from 'next/cache';
+import {isValidSignature, SIGNATURE_HEADER_NAME} from "@sanity/webhook"
+import type { NextApiRequest, NextApiResponse } from "next"
 
 type Data = {
-  message: string;
-};
+  message: string
+}
 
-const secret = process.env.SANITY_REVALIDATE_SECRET!;
-
-export async function POST(req: NextRequest) {
-  const signature = req.headers.get(SIGNATURE_HEADER_NAME) as string;
-  //.headers[SIGNATURE_HEADER_NAME]
-  const body = await readBody(req); // Read the body into a string
-
-  if (!isValidSignature(body, signature, secret)) {
-    NextResponse.json({ message: 'Invalid signature', status: 401 });
-    return;
+// Next.js will by default parse the body, which can lead to invalid signatures
+export const config = {
+    api: {
+      bodyParser: false,
+    },
   }
 
-  try {
-    const { _type: type, slug } = JSON.parse(body);
+const secret = process.env.SANITY_REVALIDATE_SECRET!
 
-    switch (type) {
-      case 'post':
-        await revalidatePath(`/projects/${slug.current}`); // The particular project
-        await revalidatePath(`/projects`); // The Projects page
-        await revalidatePath(`/`, 'page'); // The landing page featured projects
-        return NextResponse.json({ message: `Revalidated "${type}" with slug "${slug.current}"` });
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    const signature = req.headers[SIGNATURE_HEADER_NAME] as string
+    const body = await readBody(req) // Read the body into a string
+
+    if (req.method !== "POST") {
+        return res.status(401).json({ message: "Must be a POST request" })
     }
 
-    return NextResponse.json({ message: 'No managed type' });
+    if (!isValidSignature(body, signature, secret)) {
+        res.status(401).json({message: 'Invalid signature'})
+        return
+    }
+
+  try {
+    const { _type: type, slug } = JSON.parse(body)
+
+    switch (type) {
+      case "post":
+        await res.revalidate(`/projects/${slug.current}`) // The particular project
+        await res.revalidate(`/projects`)                 // The Projects page
+        await res.revalidate(`/`)                         // The landing page featured projects
+        return res.json({ message: `Revalidated "${type}" with slug "${slug.current}"` })
+    }
+
+    return res.json({ message: "No managed type" })
   } catch (err) {
-    return NextResponse.json({ message: 'Error revalidating' });
+    return res.status(500).send({ message: "Error revalidating" })
   }
 }
 
-async function readBody(readable: any) {
-  const chunks = [];
-  for await (const chunk of readable) {
-    chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk);
+async function readBody(readable:any) {
+    const chunks = []
+    for await (const chunk of readable) {
+      chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk)
+    }
+    return Buffer.concat(chunks).toString('utf8')
   }
-  return Buffer.concat(chunks).toString('utf8');
-}
