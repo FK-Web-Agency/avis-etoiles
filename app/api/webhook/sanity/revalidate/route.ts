a0dfbc73771dc81e1e28ae8e41138949042a7a83
MentorMindOtaku <contact@seckmamadou.com>
2024-02-17 05:32:58 +0100

    Refactor revalidation logic and update dependencies

--- app/api/webhook/sanity/revalidate/route.ts
+++ app/api/webhook/sanity/revalidate/route.ts
@@ -1,36 +1,41 @@
 import { isValidSignature, SIGNATURE_HEADER_NAME } from '@sanity/webhook';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { revalidatePath } from 'next/cache';
+import { NextRequest, NextResponse } from 'next/server';
 
 type Data = {
   message: string;
 };
 
-
 const secret = process.env.SANITY_REVALIDATE_SECRET!;
 
-export async function POST(req: NextApiRequest, res: NextApiResponse<Data>) {
-  const signature = req.headers[SIGNATURE_HEADER_NAME] as string;
+export async function POST(req: NextRequest) {
+  const signature = req.headers.get(SIGNATURE_HEADER_NAME) as string;
   const body = await readBody(req); // Read the body into a string
 
   if (!isValidSignature(body, signature, secret)) {
-    res.status(401).json({ message: 'Invalid signature' });
-    return;
+    return NextResponse.json({ message: 'Invalid signature' });
   }
 
   try {
     const { _type: type, slug } = JSON.parse(body);
 
-    switch (type) {
+    console.log('Revalidating', type, slug);
+
+    /* switch (type) {
       case 'post':
         await res.revalidate(`/projects/${slug.current}`); // The particular project
         await res.revalidate(`/projects`); // The Projects page
         await res.revalidate(`/`); // The landing page featured projects
         return res.json({ message: `Revalidated "${type}" with slug "${slug.current}"` });
     }
+ */
+    revalidatePath('/*');
+    return NextResponse.json({ revalidated: true, now: Date.now() });
 
-    return res.json({ message: 'No managed type' });
-  } catch (err) {
-    return res.status(500).send({ message: 'Error revalidating' });
+    //  return res.json({ message: 'No managed type' });
+  } catch (err: any) {
+    return NextResponse.json({ error: err.message });
   }
 }
 
