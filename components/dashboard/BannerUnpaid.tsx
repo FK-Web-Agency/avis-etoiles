bb20a098568aafc7dacc1b3bb991a71410050ed6
MentorMindOtaku <contact@seckmamadou.com>
2024-02-18 17:42:50 +0100

    Refactor BannerUnpaid component to use useOne hook

--- components/dashboard/BannerUnpaid.tsx
+++ components/dashboard/BannerUnpaid.tsx
@@ -1,38 +1,22 @@
 'use client';
 
-import { getSession } from '@/lib/actions/stripe.actions';
+import { useOne } from '@refinedev/core';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
 
 export default function BannerUnpaid({ sanityId }: { sanityId: string }) {
-  const [subscriber, setSubscriber] = useState<any>(null);
-  const [unpaid, setUnpaid] = useState(true);
+  const { data, isLoading } = useOne({
+    resource: process.env.NEXT_PUBLIC_SANITY_SUBSCRIBERS,
+    id: sanityId,
+  });
 
-  useEffect(() => {
-    const fetchSubscriber = async () => {
-      const subscriber = await getSession({ subscriberId: sanityId });
+  if (isLoading) return null;
 
-      if (!subscriber) {
-        return setUnpaid(false);
-      }
-
-      if (subscriber?.payment_status === 'unpaid' && subscriber?.status === 'open') {
-        return setSubscriber(subscriber);
-      }
-
-      if (subscriber?.payment_status === 'paid') {
-        return setUnpaid(false);
-      }
-    };
-
-    fetchSubscriber();
-  }, [sanityId]);
-
-  const url = subscriber?.url || 'https://billing.stripe.com/p/login/test_fZe17G2VSdtL7bG3cc';
+  const subscriber = data?.data;
+  const url = 'https://billing.stripe.com/p/login/test_fZe17G2VSdtL7bG3cc';
 
   return (
     <>
-      {subscriber && unpaid && (
+      {subscriber && subscriber?.subscription?.status !== 'active' && (
         <div className="flex flex-col justify-center items-center gap-x-6 bg-red-600 px-6 py-2.5 sm:px-3.5">
           <strong className="font-semibold block text-center text-white">Abonnement impayé</strong>
           <p className="text-sm text-center leading-6 text-white">
