fdb7247e756152760523cdfb62a15c00da0f8ba0
MentorMindOtaku <contact@seckmamadou.com>
2024-02-20 00:31:22 +0100

    Refactor Features.tsx to use a paragraph element for the subtitle

--- components/pages/home/Features.tsx
+++ components/pages/home/Features.tsx
@@ -30,9 +30,7 @@ export default function Features({ features_section }: FeaturesType) {
       <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
         <div className="max-w-screen-md mb-8 lg:mb-16">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900">{features_section?.title} </h2>
-          <div className="text-gray-500 p-light-16 sm:text-xl ">
-            {features_section?.subtitle && <PortableText value={features_section?.subtitle} />}
-          </div>
+          <p className="text-gray-500 p-light-16 sm:text-xl ">{features_section?.subtitle}</p>
         </div>
         <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
           {features_section?.features.map((feature: FeatureProps['features'], index: number) => (
