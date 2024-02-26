466888d6c2c4438709e1cc72a839fd5adf2dc865
MentorMindOtaku <contact@seckmamadou.com>
2024-02-26 20:51:17 +0100

    Update Sidebar styling to include justify-between

--- components/shared/Sidebar/index.tsx
+++ components/shared/Sidebar/index.tsx
@@ -17,7 +17,7 @@ export default function Sidebar({ children }: PropsWithChildren) {
     <>
       <div>
         {/* Static sidebar for desktop */}
-        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-60 lg:flex-col">
+        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-60 lg:flex-col justify-between">
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-600 px-6 pb-4">
             <div className="flex h-16 shrink-0 items-center">
