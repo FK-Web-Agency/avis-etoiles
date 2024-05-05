52fdd04527d9c94e065c7b7327fecf8292856881
doums85 <momoseck8@gmail.com>
2024-05-05 19:40:02 +0200

    chore: Update sign-in page with membership information and link to prices page

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,11 +1,17 @@
 import { Icons } from '@/components/shared';
 import { SignIn } from '@clerk/nextjs';
+import Link from 'next/link';
 
 export default function Page() {
   return (
     <div className="flex-center min-h-screen w-full background-body gap-4 flex-col">
       <Icons.Logo />
 
+      <h4>
+        Cette page est réservé aux membres uniquement, si vous n'êtes pas encore
+        membre vous pouvez nous rejoindre des maintenant en cliquant sur ce{' '}
+        <Link href="/prices">lien</Link>
+      </h4>
       <SignIn
         appearance={{
           variables: {
