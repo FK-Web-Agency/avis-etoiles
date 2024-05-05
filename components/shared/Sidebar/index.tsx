9d1666a59455d1d76b3c31fb953ba29363c8ceaf
doums85 <momoseck8@gmail.com>
2024-05-05 23:25:23 +0200

    refactor: Update Sidebar component to remove unnecessary CSS class and adjust navigation layout

--- components/shared/Sidebar/index.tsx
+++ components/shared/Sidebar/index.tsx
@@ -21,11 +21,11 @@ export default function Sidebar({ children }: PropsWithChildren) {
         {/* Static sidebar for desktop */}
         <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-60 lg:flex-col justify-between">
           {/* Sidebar component, swap this element with another sidebar if you like */}
-          <div className="flex grow justify-between flex-col gap-y-5 overflow-y-auto border-r border-gray-600 px-6 pb-4">
+          <div className="flex grow  flex-col gap-y-5 overflow-y-auto border-r border-gray-600 px-6 pb-4">
             <div className="flex h-16 shrink-0 items-center">
               <Icons.Logo hrefNull />
             </div>
-            <nav className="flex flex-1 flex-col">
+            <nav className="flex justify-between flex-1 flex-col">
               <ul role="list" className="flex flex-1 flex-col gap-y-7">
                 <li>
                   <NavItems />
